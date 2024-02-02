01b482da0f43bad6559eca95d60f88f7d95bdf8e
rasikhkp <raxiest@gmail.com>
2024-02-02 13:21:58 +0700

    fix: fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Space from "./pages/Space";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/space" element={<Space />} />
-			</Routes>
-		</Router>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Space />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/SlideEnterSer.tsx
+++ src/components/SlideEnterSer.tsx
@@ -4,50 +4,52 @@ import { useLayoutEffect, useRef } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
 const SlideEnterSer = ({
-	children,
-	className,
+    children,
+    className,
 }: {
-	children: any;
-	className?: string;
+    children: any;
+    className?: string;
 }) => {
-	const rootRef = useRef(null);
+    const rootRef = useRef(null);
 
-	useLayoutEffect(() => {
-		let ctx = gsap.context(() => {
-			gsap.to("#content", { opacity: 0, duration: 0 });
-			const tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: rootRef.current,
-					start: "-300% 60%",
-					markers: true,
-				},
-			});
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.to("#content", { opacity: 0, duration: 0 });
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: rootRef.current,
+                    start: "-300% 60%",
+                },
+            });
 
-			tl.to("#cover", {
-				width: "100%",
-				duration: 0.7,
-				ease: "power3.inOut",
-			})
-				.to("#content", { opacity: "100%", duration: 0 })
-				.to("#cover", {
-					marginLeft: "100%",
-					duration: 0.7,
-					ease: "power3.inOut",
-				});
-		}, rootRef);
+            tl.to("#cover", {
+                width: "100%",
+                duration: 0.7,
+                ease: "power3.inOut",
+            })
+                .to("#content", { opacity: "100%", duration: 0 })
+                .to("#cover", {
+                    marginLeft: "100%",
+                    duration: 0.7,
+                    ease: "power3.inOut",
+                });
+        }, rootRef);
 
-		return () => ctx.revert();
-	}, []);
+        return () => ctx.revert();
+    }, []);
 
-	return (
-		<div
-			className={`relative overflow-hidden w-fit ${className}`}
-			ref={rootRef}
-		>
-			<div id="content">{children}</div>
-			<div id="cover" className="absolute top-0 h-full bg-slate-200"></div>
-		</div>
-	);
+    return (
+        <div
+            className={`relative overflow-hidden w-fit ${className}`}
+            ref={rootRef}
+        >
+            <div id="content">{children}</div>
+            <div
+                id="cover"
+                className="absolute top-0 h-full bg-slate-200"
+            ></div>
+        </div>
+    );
 };
 
 export default SlideEnterSer;
